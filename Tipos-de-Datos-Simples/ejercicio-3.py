a367fd2b3c262e05840b5151386f8eaa93f8bd43
Sant-beco <sanprogramador8@gmail.com>
2025-03-14 15:18:41 -0500

    ejercicio 3

--- Tipos-de-Datos-Simples/ejercicio-3.py
+++ Tipos-de-Datos-Simples/ejercicio-3.py
@@ -1,3 +1,7 @@
 """Escribir un programa que pregunte el nombre del usuario en la consola y después de que el usuario lo introduzca muestre por pantalla 
 la cadena ¡Hola <nombre>!, donde <nombre> es el nombre que el usuario haya introducido."""
 
+print("\n Saludo personalizado")
+nombre = str(input("Ingresa tu nombre: "))
+
+print(f"Un cordial Saludo {nombre} de parte de cultural ^__^\n")
