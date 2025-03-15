785b611a21d19a19dc303df49b02176f87ac4ccf
Sant-beco <sanprogramador8@gmail.com>
2025-03-14 23:33:22 -0500

    Subiendo estructura de datos repaso para entrevista y entendiendo python ademas de practica

---
+++ Pruebas-tecnicas/estructura-datos.py
@@ -0,0 +1,28 @@
+# Listas
+
+print("\nlistas\n")
+lista = [2,4,8,16,32,64,128,256,512]
+print(lista)
+
+print("\n")
+lista_int_float = [2,6,80,100,240,12.8,2.8,6.7,8.8]
+print(lista_int_float)
+
+print("\n")
+lista_str =["Vida","life","live","Programador de codigo","que crea soluciones"]
+print(lista_str)
+
+print("\nlistas\n")
+lista_booleans = [True,False,True,True,False,False]
+print(lista_booleans)
+ 
+print("\nlistas\n")
+lista_mixta = [2,4,8,16,32,64,128,256,512,2,6,80,100,240,12.8,2.8,6.7,8.8,"Vida","life","live","Programador de codigo","que crea soluciones",True,False,True,True,False,False]
+print(lista_mixta)
+
+# imprimir todo 
+
+print(f"lista uno: {lista} \nlista dos: {lista_int_float} \nlista tres: {lista_str} \nlista cuatro: {lista_booleans} \nlista quinta: {lista_mixta}")
+
+
+print("\n")
