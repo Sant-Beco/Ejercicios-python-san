f40e7c352466f97eb062c8bfb976ccdbf829cf43
Sant-beco <sanprogramador8@gmail.com>
2025-03-22 13:33:01 -0500

    Subiendo ejercicio 1 de cadenas de texto

--- 01_Cadenas/ejercicio-1.py
+++ 01_Cadenas/ejercicio-1.py
@@ -1,3 +1,9 @@
 """Ejercicio 1
 Escribir un programa que pregunte el nombre del usuario en la consola y un número entero e imprima por pantalla en líneas 
 distintas el nombre del usuario tantas veces como el número introducido."""
+
+nombre = input("Ingrese su nombre: ")
+numero = int(input("ingrese un numero entero: "))
+
+for _ in range(numero):
+    print(nombre)
