e2b11baad6ce2e996ac9a8472f4dce6b148c407e
Sant-beco <sanprogramador8@gmail.com>
2025-03-24 09:56:26 -0500

    subiendo ejercicio 2 de cadenas

--- 01_Cadenas/ejercicio-2.py
+++ 01_Cadenas/ejercicio-2.py
@@ -3,3 +3,7 @@ Escribir un programa que pregunte el nombre completo del usuario en la consola y
 del usuario tres veces, una con todas las letras minúsculas, otra con todas las letras mayúsculas y otra solo 
 con la primera letra del nombre y de los apellidos en mayúscula. El usuario puede introducir su nombre combinando 
 mayúsculas y minúsculas como quiera."""
+
+name = input("Ingrese su nombre completo por favor: ")
+
+print(f"\n{name.upper()} \n{name.lower()} \n{name.title()}")
