fbba50b24bb0cc87f58dc6b7b902a41f073848a9
Sant-beco <sanprogramador8@gmail.com>
2025-04-01 13:33:40 -0500

    ejercicio 5 de cadenas de texto strings

--- 01_Cadenas/ejercicio-5.py
+++ 01_Cadenas/ejercicio-5.py
@@ -1,2 +1,7 @@
 """Ejercicio 5
-Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla la frase invertida."""
+Escribir un programa que pida al usuario que introduzca una frase en la consola y muestre por pantalla 
+la frase invertida."""
+
+frase = str(input("Ingrese una frase: "))
+
+print(f"{frase[::-1]}")
