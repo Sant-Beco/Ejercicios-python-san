1260d3943f96188f4beeb37b5570da0e36ea3348
Sant-beco <sanprogramador8@gmail.com>
2025-04-01 13:36:57 -0500

    ejercicio 6 de cadenas de texto strings

--- 01_Cadenas/ejercicio-6.py
+++ 01_Cadenas/ejercicio-6.py
@@ -1,3 +1,8 @@
 """Ejercicio 6
-Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y después muestre por pantalla 
-la misma frase pero con la vocal introducida en mayúscula."""
+Escribir un programa que pida al usuario que introduzca una frase en la consola y una vocal, y 
+después muestre por pantalla la misma frase pero con la vocal introducida en mayúscula."""
+
+frase = str(input("Ingrese una frase: "))
+vocal = str(input("Ingrese una vocal: "))
+
+print(f"{frase} {vocal.upper()}")
