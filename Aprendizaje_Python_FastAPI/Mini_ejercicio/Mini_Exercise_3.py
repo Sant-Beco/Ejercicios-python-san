c7967964ee2484de2f67e466473ee8d672595c64
Sant-beco <sanprogramador8@gmail.com>
2025-04-12 13:34:49 -0500

    subiendo mini ejercicio 3 mejorado

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_3.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_3.py
@@ -1,9 +1,50 @@
 """🧠 Mini reto (en inglés):
 ¿Quieres practicar un poco más con condicionales?"""
 
-age = int(input("\nIngrese su edad: "))
+# age = int(input("\nIngrese su edad: "))
 
-if age >= 18:
-    print("You are a adult\n")
-else:
-    print("You are a minor\n")
+# if age >= 18:
+#     print("You are an adult\n")
+# else:
+#     print("You are a minor\n")
+
+
+# version dos
+
+
+print("\n📋 Choose what you want to enter:")
+print("1. Age")
+print("2. Height")
+print("3. Name")
+
+option = ""
+
+while option != 4:
+    print("\n📋 Choose what you want to enter:")
+    print("1. Age")
+    print("2. Height")
+    print("3. Name")
+    print("4. Exit")
+    
+    option = input("Enter option (1-4): ")
+
+    if option == "1":
+        age = int(input("Enter your age: "))
+        print(f"You are {age} years old.")
+
+    elif option == "2":
+        height = float(input("Enter your height in meters: "))
+        print(f"Your height is {height} meters.")
+
+    elif option == "3":
+        name = input("Enter your name: ")
+        print(f"Hello, {name}!")
+
+    elif option == "4":
+        print("Goodbye hasta pronto")
+        break
+
+    else:
+        print("❌ Invalid option. Please enter 1, 2, or 3.")
+
+    print()
