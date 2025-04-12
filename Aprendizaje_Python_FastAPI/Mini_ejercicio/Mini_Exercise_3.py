7c143fcf8e61c4c8ae463c5a9ababa9ee14ef315
Sant-beco <sanprogramador8@gmail.com>
2025-04-12 11:49:11 -0500

    Subiendo mini reto de condicional

---
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_3.py
@@ -0,0 +1,9 @@
+"""🧠 Mini reto (en inglés):
+¿Quieres practicar un poco más con condicionales?"""
+
+age = int(input("\nIngrese su edad: "))
+
+if age >= 18:
+    print("You are a adult\n")
+else:
+    print("You are a minor\n")
