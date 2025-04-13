021aebf2776dc24d22fa0a6267b7349020279cfb
Sant-beco <sanprogramador8@gmail.com>
2025-04-13 18:04:14 -0500

    subiendo mini exercise 5

---
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_5.py
@@ -0,0 +1,7 @@
+name = input("What is your name: ")
+age = int(input("What is your Age: "))
+favorite_number = int(input("How is your favorite number: "))
+wheather_student = input("What is studing: ")
+hobbits_tree = (input("write three hobbits: ")).split()
+
+print(f"Hi {name} have {age} years old. your favorite number is {favorite_number} and wheater student {wheather_student} and hobbits are {hobbits_tree}")
