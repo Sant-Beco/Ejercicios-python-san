c3708148ec1b0deff2096f4b5cfcd6939053036e
Sant-beco <sanprogramador8@gmail.com>
2025-04-17 16:57:35 -0500

    subiendo mini ejercicio 6

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_6.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_6.py
@@ -1,3 +1,16 @@
 """🎯 Goal:
 Create a program that asks the user's age and tells them what kind of movie they can watch."""
 
+age = int(input("Ingrese su edad: "))
+
+if age < 0:
+    print("❌ Invalid age. Please try again.") 
+
+elif age <= 12:
+    print("You can watch cartoons 🎨")
+
+elif 17 <= age >= 13:
+    print("You can watch teen movies 🍿")
+
+elif age >= 18:
+    print("You can watch all movies 🎥")
