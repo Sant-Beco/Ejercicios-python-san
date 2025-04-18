91177ffa79d8cf5f1a8f9040d705925aafb462c5
Sant-beco <sanprogramador8@gmail.com>
2025-04-18 17:31:52 -0500

    Subiendo mini ejercisio 7 listo

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_7.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_7.py
@@ -2,9 +2,22 @@
 🎯 Goal:
 Ask the user for their test score and tell them what grade they got."""
 
-score = int(input('Ingrese su score: '))
+score = int(input('Enter your test score: '))
 
-if 100 <= score >= 90:
-    print(f'\nEnter your test score: {score}\nExcellent work! 🏆 You got a A.')
+if 90 <= score <= 100:
+    print(f'\nExcellent work! 🏆 You got a A.')
 
-    
+elif 80 <= score <= 89:
+    print(f'\nGreat job! 👏 You got a B')
+
+elif 70 <= score <= 79:
+    print(f'\nGood effort! 👍 You got a C')
+
+elif 60 <= score <= 69:
+    print(f'\nYou passed, but study more. You got a D')
+
+elif score < 60:
+    print(f'\nYou failed. Try again 💪 You got a E')
+
+else:
+    print('❌ Invalid score.')
