4e6aa8bba797d84393d962594dddc899af8815f1
Sant-beco <sanprogramador8@gmail.com>
2025-04-18 06:50:28 -0500

    subiendo mini exercise 7 falta

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_7.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_7.py
@@ -1,3 +1,10 @@
 """🧪 Mini Exercise 7: 🏫 Grade Evaluator
 🎯 Goal:
 Ask the user for their test score and tell them what grade they got."""
+
+score = int(input('Ingrese su score: '))
+
+if 100 <= score >= 90:
+    print(f'\nEnter your test score: {score}\nExcellent work! 🏆 You got a A.')
+
+    
