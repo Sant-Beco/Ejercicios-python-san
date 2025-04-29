035784e40320b3d6fcd271c9860f5afb25a48974
Sant-beco <sanprogramador8@gmail.com>
2025-04-29 05:14:39 -0500

    Subiendo mini ejercicio 10 listo

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_10.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_10.py
@@ -2,3 +2,25 @@
 🎯 Goal:
 Create a program that asks for your name, age, country, and favorite color.
 Store this information in a dictionary and print it in a friendly message."""
+
+try:
+    name = input("Enter your name: ")
+    age = int(input("Enter your age old: "))
+    country = input("Enter your country: ")
+    favorite_color = input("Enter your favorite color: ")
+
+    diccionario = {
+        "name": name,
+        "age": age,
+        "country": country,
+        "favorite_color": favorite_color
+    }
+
+    print(f"Your dates is {diccionario} happy day")
+
+    print(f"\nHello {name}! 👋")
+    print(f"You are {age} years old, live in {country}, and your favorite color is {favorite_color}. 🎨\n")
+    print(f"\nremenber that save in dictionary {diccionario}\n")
+
+except ValueError:
+    print("❌ Age must be a number. Please try again.")
