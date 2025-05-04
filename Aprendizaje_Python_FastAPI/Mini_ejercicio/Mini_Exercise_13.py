b69a708528372dea2e8244bb9763ba3323175ef6
Sant-beco <sanprogramador8@gmail.com>
2025-05-04 04:06:56 -0500

    subiendo exercise 13 listo falta try excet

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_13.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_13.py
@@ -3,12 +3,21 @@
 Create a program that asks the user to enter their 3 favorite things (like food, hobbies, or colors), stores them in a list, and prints them in a sentence.
 
 Example output:
-sql
-Copiar
-Editar
+
 Enter your first favorite thing: pizza
 Enter your second favorite thing: music
 Enter your third favorite thing: soccer
 
 Your favorite things are: ['pizza', 'music', 'soccer']
 You love pizza, music, and soccer!"""
+
+thing_one = input('Enter your first favorite thing: ')
+thing_two = input('Enter your second favorite thing:    ')
+thing_three = input('Enter your third favorite thing:   ')
+
+lista = []
+
+lista.extend([thing_one, thing_two, thing_three])
+
+print(f"\nYour favorite things are: {lista}")
+print(f"You love {thing_one}, {thing_two}, and {thing_three}!")
