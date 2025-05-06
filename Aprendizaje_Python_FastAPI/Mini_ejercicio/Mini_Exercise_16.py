13714d93514b4c49de224f1cec480c111f9213b2
Sant-beco <sanprogramador8@gmail.com>
2025-05-06 03:53:17 -0500

    Subiendo el exersice 16 falta mensajes

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_16.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_16.py
@@ -17,3 +17,13 @@ La diferencia entre set_a y set_b.
 
 La diferencia simétrica entre set_a y set_b."""
 
+set_a = {1,2,3,4,5}
+
+set_b ={4,5,6,7,8}
+
+print(set_a.union(set_b))
+print(set_a.intersection(set_b))
+print(set_a.difference(set_b))
+print(set_a.symmetric_difference(set_b))
+
+
