67c1b539a8f122a5980fbe817652f800f9d081b8
Sant-beco <sanprogramador8@gmail.com>
2025-05-08 04:06:34 -0500

    subiendo exersice 15 empece terminar

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_15.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_15.py
@@ -1,3 +1,18 @@
 """🧪 Mini Ejercicio: Calculadora Segura
 🎯 Objetivo:
 Crear una función que sume dos números, manejando errores si el usuario escribe mal los datos."""
+
+
+print("🎯 Calculadora")
+num_one = int(input('Ingrese un numero: '))
+num_two = int(input('Ingrese un numero: '))
+
+suma = num_one + num_two
+
+print(f"\nSuma = {suma}")
+print(f"\nSuma = {num_one + num_two}")
+print(f"\nSuma = {num_one - num_two}")
+print(f"\nSuma = {num_one * num_two}")
+print(f"\nSuma = {num_one / num_two}")
+print(f"\nSuma = {num_one // num_two}")
+
