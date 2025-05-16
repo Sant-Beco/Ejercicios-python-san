6f0f3e410e7e042e715d0af8ee5b3c42b07168e5
Sant-beco <sanprogramador8@gmail.com>
2025-05-16 03:57:34 -0500

    subi un ejercicio para practicar

---
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_17.py
@@ -0,0 +1,12 @@
+""" 1. Validador de contraseña segura
+Objetivo: Verificar si una contraseña cumple con los requisitos de seguridad.
+
+📌 Requisitos:
+
+Mínimo 8 caracteres
+
+Al menos 1 mayúscula
+
+Al menos 1 minúscula
+
+Al menos 1 número"""
