be1c70bca6a276e4afeee1d3c817d7771d26143a
Sant-beco <sanprogramador8@gmail.com>
2025-05-24 03:48:50 -0500

    Comence reto exercise 20

--- Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_20.py
+++ Aprendizaje_Python_FastAPI/Mini_ejercicio/Mini_Exercise_20.py
@@ -4,3 +4,26 @@
 
 # Aquí empieza tu código:
 """
+
+class Auto:
+    def __init__(self, marca, color, modelo):
+        self.marca = marca
+        self.color = color
+        self._modelo = modelo
+
+    # Método normal
+    def decir_modelo(self):
+        print(f"{self.marca} Toyota tornado")
+
+    def describir_auto(self):
+        print(f"Este auto es un {self.marca} de color {self.color}.")
+
+
+mi_auto = Auto("nissan","negro")
+
+
+mi_auto.decir_modelo()
+mi_auto.describir_auto()
+
+print(type(Auto))
+print(type(mi_auto))
