3f87f2dfb22abb48f574d1a1927c9fb2fa0d8267
Sant-beco <sanprogramador8@gmail.com>
2025-08-14 17:26:52 -0500

    adelantando dia 2 ejercicio

---
+++ Reto-90-dias/dia2/ejercicio_practica.py
@@ -0,0 +1,19 @@
+# import sys
+
+# edad = int(input("Ingresa tu edad: "))
+
+# if edad < 18:
+#     print(f"Tienes {edad} eres menor de edad")
+# elif edad == 18:
+#     print(f"Tienes {edad} justo 18 años tienes")
+# elif edad > 18:
+#     print(f"Tienes {edad} eres MAYOR de edad")
+# else:
+#     print("Error vuelve a ejecutar")    
+
+
+# if __name__ == "__main__":
+#     if len(sys.argv) >= 2:
+#         edad(sys.argv[1], sys.argv[2], sys.argv[3])
+#     else:
+#         edad()
