bd27508c8ecfbb193d0626d88cf45e8465eed696
Sant-beco <sanprogramador8@gmail.com>
2025-08-15 19:56:00 -0500

    subiendo adelanto sigue pensando

--- Reto-90-dias/dia2/ejercicio_practica.py
+++ Reto-90-dias/dia2/ejercicio_practica.py
@@ -17,3 +17,18 @@
 #         edad(sys.argv[1], sys.argv[2], sys.argv[3])
 #     else:
 #         edad()
+
+import sys
+
+nombre = input("Escribe tu nombre: ")
+edad = int(input("Ingrese su edad: "))
+
+if edad < 18:
+    print("Menor de edad")
+elif edad == 18:
+    print("tienes 18")
+elif edad > 18:
+    print("eres mayor de edad")
+
+for numero in range(1,edad):
+    print(numero)
