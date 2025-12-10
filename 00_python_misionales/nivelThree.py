939ccd0d928c336f454cc09135e46e4c5b2d0810
Sant-beco <sanprogramador8@gmail.com>
2025-12-10 17:30:11 -0500

    Subiendo cambios dia 1 listo

---
+++ 00_python_misionales/nivelThree.py
@@ -0,0 +1,20 @@
+visitas = 500
+ventas = 125
+
+print(f"{ventas / visitas * 100}%")
+
+# 3. Mini reto
+
+# Un video en TikTok tuvo:
+
+# 20,000 vistas
+
+# 4000 likes
+
+# ¿Cuál es el porcentaje de likes?
+
+
+likes = 4000
+visitas_video = 20000
+
+print(f"{likes / visitas_video * 100}%")
