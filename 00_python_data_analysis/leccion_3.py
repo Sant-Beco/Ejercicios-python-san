90fd8c4c586492c3c66ace5af8d703edc9c0460d
Sant-beco <sanprogramador8@gmail.com>
2025-12-31 17:39:54 -0500

    subiendo correccion de funcion return

--- 00_python_data_analysis/leccion_3.py
+++ 00_python_data_analysis/leccion_3.py
@@ -4,15 +4,17 @@
 
 def evaluar_calidad_huevo (peso_promedio, porcentaje_defectos):
     if peso_promedio > 60 and porcentaje_defectos < 5:
-        print(f"Calidad Alta")
-    elif 55 > peso_promedio < 60:
-        print(f"Calidad Media")
+        return "Calidad Alta"
+    elif 55 <= peso_promedio <= 60:
+        return "Calidad Media"
     elif peso_promedio < 55 or porcentaje_defectos > 10:
-        print(f"Calidad Baja")
+        return f"Calidad Baja"
     else:
-        print("ups algo salio mal")
+        return "ups datos inconsientes"
 
 
 resultado = evaluar_calidad_huevo(64, 4)
+print(resultado)
+
 resultado = evaluar_calidad_huevo(54, 11)
 print(resultado)
