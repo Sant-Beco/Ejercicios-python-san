f8fb8dc9113d83bcf43b485de96a29ecf3b8db5a
Sant-beco <sanprogramador8@gmail.com>
2026-01-12 10:40:43 -0500

    Subiendo ejercicio 6

---
+++ 00_python_data_analysis/leccion_6.py
@@ -0,0 +1,36 @@
+"""🔥 LECCIÓN 6 – FUNCIONES QUE ANALIZAN DATOS Y GENERAN CRITERIOS
+
+necesito saber la produccion maxima, el promedio, produccion 
+
+¿qué es más peligroso: promedio bajo o mucha variación?
+
+promedio bajo, porque si la variacion es superior al promedio minimo aceptable esta bien
+
+¿La producción fue constante o muy variable?
+
+¿El promedio cumple el mínimo esperado?
+
+¿Hubo días críticos?
+"""
+
+
+
+def control_produccion(produccion_diaria):
+    produccion_maximo = max(produccion_diaria)
+    produccion_minima_aceptable = 80
+    produccion_promedio = sum(produccion_diaria) / len(produccion_diaria)
+    if produccion_minima_aceptable > produccion_promedio:
+        produccion_minima_semana = min(produccion_diaria)
+        return f"Alerta, la produccion mas critica {produccion_minima_semana}"
+    elif produccion_minima_aceptable < produccion_promedio:
+        return "Produccion constante"
+    elif produccion_promedio > produccion_maximo:
+        return "produccion muy alta alta algo sucede"
+    else:
+        return "Ingresaste un dato no valido"
+    
+
+print(control_produccion([82, 85, 83, 84, 81, 83, 82]))
+print(control_produccion([82, 45, 83, 35, 81, 83, 82]))
+print(control_produccion([82, 85, 83, 84, 81, 83, 82]))
+print(control_produccion([56, 78, 83, 84, 76, 83, 82,]))
