d0071a336abdd0314159aa96ac28094c4d02c538
Sant-beco <sanprogramador8@gmail.com>
2026-01-27 18:10:56 -0500

    Subiendo algoritmos falta un poco

---
+++ 02_Fundamentos_Irrompibles/Algoritmos.py
@@ -0,0 +1,68 @@
+"""Exersice #1: Name of variables
+
+produccion_diaria = number
+produccion_minima = condicional o operacion
+temperatura = number
+resultado= responde una pregunta o cuestion
+
+Exersice #2: 
+
+escribe la produccion de los ultimos 7 dias y la de hoy
+evalua cual es produccion promedio
+evalua cual es la produccion minima
+evalua cual es la produccion maxima
+si la produccion del dia es mas del promedio o del maximo es buena
+
+Exersice #3:
+
+Si producción < 83 → dia bajo
+
+Si temperatura > 24 → peligro
+
+Si ambas pasan → alerta en produccion
+
+Exersice #4:
+
+Enunciado
+
+Producción mínima aceptable: 80
+
+Responde:
+
+¿Es lo mismo producir 79 que 40? no es lo mismo, porque 79 esta 1 punto por debajo del minimo aceptable 
+y 40 esta 40 puntos por debajo y es mucha diferencia. pero en terminos generales los dos estan por 
+debajo del promedio minimo aceptable pero no es lo mismo
+
+¿Por qué un promedio puede engañar?
+
+si es muy alto, o muy bajo, los extremos en la vida real no es normal, porque el promedio toma todos 
+los datos y si hay un dia bajo el promedio igual y asi es mayor, y tampoco se sabe si esta lejos
+o cerca de una regla exacta
+
+¿Qué dato adicional te ayudaría a decidir mejor?
+
+produccion maxima
+produccion de los ultimos 8 dias
+numero de aves hembras
+
+
+exersice #5
+
+Producción semanal:
+
+[82, 79, 78, 85, 81, 77, 83]
+
+
+✍️ Responde:
+
+¿Qué estás buscando?
+ordenar del dia mejor a peor para analizar mejor la informacion
+
+¿Cuándo debes contar?
+el dia menor y mayor e ir comparando y ordenando
+
+¿Cuándo debes alertar?
+si hay un dia muy alto o bajo, 2 o 3 punto debajo del 81 es alerta o superior a 86
+
+
+"""
