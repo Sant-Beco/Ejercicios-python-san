ad449ca2b903877464eb0dca0b95de33b56cf109
Sant-beco <sanprogramador8@gmail.com>
2026-01-28 19:05:54 -0500

    Subiendo final de algoritmos seguimos con varibles dia 2

---
+++ 02_Fundamentos_Irrompibles/Algoritmos_dos.py
@@ -0,0 +1,78 @@
+# ejercicio 6 👉 Cepillarte los dientes
+
+"""Voy al lavamanos
+saco el cepillo
+saco la crema dental
+abro canilla
+hecho agua
+cierro canilla
+hecho la crema dental sobre el cepillo
+introdusco el cepillo en mi boca
+hago movimientos para cepillar dientes
+cepillo lengua 
+abro canilla
+enjuago con agua
+cierro canilla
+guardo cepillo y crema dental"""
+
+# Ejercicio 7 ecidir si comprar algo
+
+"""busco direccion de tienda
+salgo de casa
+contrato indrive
+llegamos a tienda
+entro a la tienda
+busco una chaqueta negra
+veo opciones y precios
+si es menor el precio de 250.000
+Lo compro 
+sino no compro busco otra tienda 
+repito proceso hasta comprar chaqueta"""
+
+# Mini-reto final 🧩
+"""Reto (SIN código):
+
+Explícame con tus palabras:
+
+¿Qué es un algoritmo?
+instrucion de pasos consecutivos que llevan hacer un proceso o solucion a un problema
+
+¿Por qué el orden importa?
+porque si un paso esta mal planteado o no se hace deja de funcionar y falla
+
+Dame un ejemplo cotidiano
+
+ver instagram
+
+enciendo telefono
+desbloqueo telefono
+busco icono de instagram
+abro instagram
+ver reels
+
+¿Por qué el algoritmo va antes del código?
+
+porque es importante entender que hacer, que es importante tener en cuenta y
+el problema o proceso a realizar
+
+si necesitas comer el dulce que mas te gusta primero necesitas pensar 
+en si tus papas de lo van a comprar, comportate bien y pidelos para lograrlo
+
+📌 Escríbelo como si se lo explicaras a un niño de 10 años."""
+
+
+# Antes de avanzar (obligatorio)
+"""Respóndeme SOLO esto, con tus palabras:
+
+1️⃣ ¿Qué es una variable?
+un tipo de dato, una variable es como una casa vacia que puede contener diferentes 
+partes, combiar y almacenar
+2️⃣ ¿Por qué una variable no debe representar “muchas cosas”?
+porque puede variar en sus cantidades pero no en tipo, si son muchas cosas ya 
+seria una estructura de datos como lista, dupla o diccionario
+3️⃣ Dame un ejemplo de mal nombre de variable y por qué rompe la lógica
+1importante
+2nosequehago
+0adgsakja
+no es buena  practica que empuece el nombre con numeros y no se entiende para que
+sirve o que almacena"""
