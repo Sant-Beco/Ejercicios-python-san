2f0b19adcd0a29909730a4ca437165d83d36d200
Sant-beco <sanprogramador8@gmail.com>
2026-01-29 19:02:01 -0500

    Subiendo 01 variables

---
+++ 02_Fundamentos_Irrompibles/01_variables.py
@@ -0,0 +1,31 @@
+#🧠 Ejercicio A
+
+"""Piensa variables para una granja:
+
+Producción del día
+produccion_diaria = 82
+
+Producción mínima aceptable
+produccion_minima_aceptable
+
+Temperatura del galpón
+Temperatura_galpon
+
+Estado de alerta
+Estado_alerta
+
+👉 Solo nombres, sin código."""
+
+#🧠 Ejercicio B
+
+"""¿Cuál está mal y por qué?
+
+x mal porque no se sabe que representa x
+
+produccion puede ser bien pero falta definicion
+
+produccion_diaria esta bien se da a entender sola
+
+pd que es pd esta mal"""
+
+#Ejercicios prácticos (Python)
