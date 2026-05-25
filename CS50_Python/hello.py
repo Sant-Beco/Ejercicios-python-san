ad72dba26898bba9cadeb71a2e0f1790d9657f48
Sant-beco <sanprogramador8@gmail.com>
2026-05-25 04:04:44 -0500

    subiendo dia 1, video 31 minutos week 0

--- CS50_Python/hello.py
+++ CS50_Python/hello.py
@@ -1,4 +1,10 @@
+#Ask user for their name
+
 print("Hello, world")
 
-
 # print("Hello, world"      Bug syntaxError
+
+name = input("Enter your name: ")
+print("Hello, " + name)
+print("Hello, ", name)
+print(f"Hello, {name} your welcome of course!")
