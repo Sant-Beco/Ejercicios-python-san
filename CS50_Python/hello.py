822c6897b95fcf56454e4e34c6a410e5b95be09a
Sant-beco <sanprogramador8@gmail.com>
2026-05-27 04:22:26 -0500

    leyendo y ejecutando ejemplos

--- CS50_Python/hello.py
+++ CS50_Python/hello.py
@@ -8,3 +8,13 @@ name = input("Enter your name: ")
 print("Hello, " + name)
 print("Hello, ", name)
 print(f"Hello, {name} your welcome of course!")
+
+# DEFINIR función
+def calcular_total(precio, cantidad):
+    # parámetros: precio, cantidad
+    total = precio * cantidad
+    return total  # devuelve el resultado
+
+# LLAMAR función
+resultado = calcular_total(10, 5)  # resultado = 50
+print(f"Funcion de calcular precio {resultado}")
